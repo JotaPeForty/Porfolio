7be90f7de4f6a6508475279a5919c7bcb46da462
Juan Pablo Benavente <juanp.benavente@gmail.com>
2022-01-10 23:01:09 -0300

    acerca de mi y habilidades

--- src/Components/AcercaDeMi.jsx
+++ src/Components/AcercaDeMi.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import '../Style/AcercaDeMi.scss'
+import '../Style/AcercaDeMi.scss';
+import dev from "../Img/developer.png"
 
 const AcercaDeMi = () => {
     return (
         <div className="container_ADM" id="AcercaDeMi">
+        <div className="cont_img"><img src={dev} alt="dev"/></div>
+        <div className="cont_text">
             <h1>Acerca de mi</h1>
+            <p>👉Hola, mi nombre es Juan Pablo, soy Desarrollador Full Stack y Fotógrafo. Retome el camino a este maravilloso mundo IT, después de pasar 14 años en un rubro diferente, el cual me dio la experiencia de transformar un sector de la empresa y formar un equipo de trabajo excelente como así también el desafío diario de la resolución de problemas. Siento que es momento de poner mi experiencia junto todos mis conocimientos a disposición de quien la requiera en esta nueva etapa.</p>
+            <p>Comencé aprendiendo lo básico, luego reparación de pc y redes, ahí nomas descubrí el diseño y aprendí de forma autodidacta algunas herramientas, hasta mi primer curso de diseño web. Luego curse la mitad de la carrera de Técnico Sup. en fotografía, que me dio mas herramientas y conocimientos en diseño, y hoy ya con conocimientos más avanzados(JavaScript, React, Redux, NodeJs, Express, PosgreSQL, HTML5, CSS3, Sass) y todo un mundo y lenguajes por seguir descubriendo.</p>
+            <p>Hoy me encuentro a las puertas de una nueva etapa de mi vida, de reunir todo lo aprendido y poder crecer en este mundo IT, por eso estoy en búsqueda de ese grupo humano para crecer más, y juntos…</p>
+            <p>Si crees que soy lo que estas buscando….</p>
+            </div>
         </div>
     )
 }
--- src/Components/Habilidades.jsx
+++ src/Components/Habilidades.jsx
@@ -1,12 +1,116 @@
-import React from 'react'
-import "../Style/Habilidades.scss"
+import React from "react";
+import html from "../Img/icons/html5.png";
+import css from "../Img/icons/CSS3.png";
+import js from "../Img/icons/JavaScript.png";
+import react from "../Img/icons/reactjs.png";
+import redux from "../Img/icons/Redux.png";
+import node from "../Img/icons/Node.js.png";
+import express from "../Img/icons/expressjs.png";
+import postgres from "../Img/icons/psql.png";
+import sequelize from "../Img/icons/sequelize.png";
+import sass from "../Img/icons/sass.png";
+import git from "../Img/icons/git.png";
+import cert from "../Img/Certificate.png";
+import "../Style/Habilidades.scss";
 
 const Habilidades = () => {
-    return (
-        <div className="container_habilidades" id="Habilidades">
-           <h1>Habilidades</h1> 
-        </div>
-    )
-}
+  return (
+    <div className="container_habilidades" id="Habilidades">
+      <div className="cont_icons">
+        <h1>Lenguajes y herramientas</h1>
+        <table>
+          <tr>
+            <td>
+              <a href="https://html5.org/">
+                <img src={html} alt="HTML5" />
+              </a>
+              <br />
+              HTML 5
+            </td>
+            </tr>
+            <tr>
+            <td>
+              <a href="https://developer.mozilla.org/es/docs/Web/CSS">
+                <img src={css} alt="CSS" />
+              </a>
+              <br />
+              CSS 3
+            </td>
+            <td>
+              <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+                <img src={js} alt="Javascript" />
+              </a>
+              <br />
+              JavaScript
+            </td>
+            </tr>
+            <tr>
+            <td>
+              <a href="https://reactjs.org/">
+                <img src={react} alt="ReactJS" />
+              </a>
+              <br />
+              ReactJS
+            </td>
+            <td>
+              <a href="https://redux.js.org/">
+                <img src={redux} alt="Redux" />
+              </a>
+              <br />
+              Redux
+            </td>
+            <td>
+              <a href="https://nodejs.org/en/">
+                <img src={node} alt="NodeJS" />
+              </a>
+              <br />
+              NodeJS
+            </td>
+            </tr>
+            <tr>
+            <td>
+              <a href="https://expressjs.com/">
+                <img src={express} alt="ExpressJS" />
+              </a>
+              <br />
+              ExpressJS
+            </td>
+            <td>
+              <a href="https://www.postgresql.org/">
+                <img src={postgres} alt="Postgresql" />
+              </a>
+              <br />
+              PostgreSQL
+            </td>
+            <td>
+              <a href="https://sequelize.org/">
+                <img src={sequelize} alt="sequelize" />
+              </a>
+              <br />
+              Sequelize
+            </td>
+            <td>
+              <a href="https://git-scm.com/">
+                <img src={sass} alt="git" />
+              </a>
+              <br />
+              Sass
+            </td>
+            <td>
+              <a href="https://git-scm.com/">
+                <img src={git} alt="git" />
+              </a>
+              <br />
+              Git
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div>
+        <img src={cert} alt="certificate"/>
+      </div>
+    </div>
+  );
+};
 
-export default Habilidades
+export default Habilidades;
