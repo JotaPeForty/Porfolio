4464ca573eb42e1a8c1e01ac3ed26a1c8acd37a6
Juan Pablo Benavente <juanp.benavente@gmail.com>
2022-01-12 00:55:59 -0300

    Update Habilidades.jsx

--- src/Components/Habilidades.jsx
+++ src/Components/Habilidades.jsx
@@ -10,7 +10,7 @@ import postgres from "../Img/icons/psql.png";
 import sequelize from "../Img/icons/sequelize.png";
 import sass from "../Img/icons/sass.png";
 import git from "../Img/icons/git.png";
-import cert from "../Img/certificate.png";
+import cer from "../Img/certificate.png";
 import {FormattedMessage} from 'react-intl';
 import "../Style/Habilidades.scss";
 
@@ -108,7 +108,7 @@ const Habilidades = () => {
         </table>
       </div>
       <div>
-        <img src={cert} alt="certificate"/>
+        <img src={cer} alt="certificate"/>
       </div>
     </div>
   );
